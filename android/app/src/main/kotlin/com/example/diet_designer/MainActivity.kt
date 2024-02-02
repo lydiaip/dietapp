e70a6cee36201ef0ef4a7d5981653a0cc0f691fd
lydiaip <lydiaiyamu1234@hotmail.co.uk>
2024-02-02 15:57:03 +0000

    deleted everything and start build step by step

--- android/app/src/main/kotlin/com/example/diet_designer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.diet_designer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/shared/shared.dart
+++
@@ -1,4 +0,0 @@
-export 'logo.dart';
-export 'popup_messenger.dart';
-export 'drawer_menu.dart';
-export 'bottom_navbar_menu.dart';
